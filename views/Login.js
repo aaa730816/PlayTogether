bdf7e8a82aede1e87166c02ca243ade13e809d86
aaa730816 <247238913@qq.com>
2018-03-16 23:25:18 +0800

    3.16

--- views/Login.js
+++ views/Login.js
@@ -1,28 +1,31 @@
-import React,{Component} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import * as QQAPI from 'react-native-qq';
 import IconFA from 'react-native-vector-icons/FontAwesome';
 export default class Login extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            qq:'qq'
+        this.state = {
+            qq: 'qq'
         }
     }
     render() {
         return (
             <View>
-                <TouchableOpacity onPress={()=>this.qqLogin}>
-                <IconFA name='qq' size={30}></IconFA>
+                <TouchableOpacity onPress={() => this.qqLogin()}>
+                    <IconFA name='qq' size={30}></IconFA>
                 </TouchableOpacity>
                 <Text>{this.state.qq}</Text>
             </View>
         )
     }
     qqLogin = () => {
-        QQAPI.login().then(function(result) {
-            this.setState({
-                qq:result
+        var _this = this;
+        QQAPI.login().then(function (result) {
+            _this.setState(() => {
+                return {
+                    qq: result.access_token
+                }
             })
         })
     }
